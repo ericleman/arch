7e5aec5ee821e76d11b6ea6e584c222d81630b81
ericleman <eric.leman@gmail.com>
2022-01-06 17:09:26 +0100

    Delete 610-vscodium-personal-settings.sh

--- arch_install/chroot_install/610-vscodium-personal-settings.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "################################################################"
-echo "############ 610-vscodium-personal-settings.sh #################"
-echo "################################################################"
-
-su eric -c "mkdir -p /home/eric/.config/VSCodium/User"
-su eric -c "cp /chroot_install/personal/VSCodium/settings.json /home/eric/.config/VSCodium/User/settings.json"
