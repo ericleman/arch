0f6e5bf27c4ba528b215e8504a6483b6e83ccf64
ericleman <eric.leman@gmail.com>
2022-01-06 18:20:40 +0100

    Update 100-gnome-shell.sh

--- arch_install/chroot_install/100-gnome-shell.sh
+++ arch_install/chroot_install/100-gnome-shell.sh
@@ -4,8 +4,9 @@ echo "##################  100-gnome-shell.sh #########################"
 echo "################################################################"
 
 pacman -Syyu --noconfirm
-pacman -S gnome dconf dconf-editor gnome-weather gnome-tweaks --noconfirm --needed
-# I choose not to install gnome-extra but just: dconf dconf-editor gnome-weather gnome-tweaks
+pacman -S gnome gnome-extra --noconfirm --needed
+# If I choose not to install gnome-extra but just: dconf dconf-editor gnome-weather gnome-tweaks, then:
+# pacman -S gnome dconf dconf-editor gnome-weather gnome-tweaks --noconfirm --needed
 
 systemctl enable gdm.service
 
