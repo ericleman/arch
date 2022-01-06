1e34cbde73767283b46242c65e2af5801084bfe7
ericleman <eric.leman@gmail.com>
2022-01-06 17:07:19 +0100

    Delete 500-gnome-settings.sh

--- arch_install/chroot_install/500-gnome-settings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "################################################################"
-echo "##################### 500-gnome-settings.sh ####################"
-echo "################################################################"
-
-sudo -u eric dbus-launch dconf load / < /chroot_install/personal/gnome-settings.dconf
-
-
