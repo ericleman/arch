2bc8bebda8e13c58456a46c444d8466d12f0abc9
ericleman <eric.leman@gmail.com>
2022-01-06 17:24:28 +0100

    Delete 650-octopi-personal-settings.sh

--- arch_install/chroot_install/650-octopi-personal-settings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "################################################################"
-echo "############### 650-octopi-personal-settings.sh #################"
-echo "################################################################"
-
-
-su eric -c "mkdir -p /home/eric/.config/octopi"
-su eric -c "cp /chroot_install/personal/octopi/octopi.conf /home/eric/.config/octopi/octopi.conf"
-
