0f19a9ee0c14a9277bbb8e233193159de64208ec
ericleman <eric.leman@gmail.com>
2022-01-06 17:07:37 +0100

    Update chroot.sh

--- arch_install/chroot_install/chroot.sh
+++ arch_install/chroot_install/chroot.sh
@@ -120,7 +120,7 @@ if [ "$HARDWARE" == "vmware" ]; then /chroot_install/130-vmware.sh; fi
 /chroot_install/200-software.sh $DESKTOP
 /chroot_install/300-AUR.sh $DESKTOP
 
-if [ "$DESKTOP" == "gnome" ]; then /chroot_install/500-gnome-settings.sh; fi
-if [ "$DESKTOP" == "kde" ]; then /chroot_install/500-kde-settings.sh; fi
-/chroot_install/600-other-personal-settings.sh
+# if [ "$DESKTOP" == "gnome" ]; then /chroot_install/500-gnome-settings.sh; fi
+# if [ "$DESKTOP" == "kde" ]; then /chroot_install/500-kde-settings.sh; fi
+# /chroot_install/600-other-personal-settings.sh
 
